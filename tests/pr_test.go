b16ba70503e39d07b278d04c2d0dc4b81eed34f4
ocofaigh <ocofaigh@ie.ibm.com>
2024-06-21 10:03:16 +0100

    feat: add COS layer

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -6,6 +6,9 @@ import (
 	"testing"
 )
 
+// Use existing resource group
+const resourceGroup = "geretain-test-resources"
+
 func TestProjectsFullTest(t *testing.T) {
 
 	options := testprojects.TestProjectOptionsDefault(&testprojects.TestProjectsOptions{
@@ -16,7 +19,7 @@ func TestProjectsFullTest(t *testing.T) {
 
 	options.StackInputs = map[string]interface{}{
 		"prefix":                       options.Prefix,
-		"resource_group_name":          options.Prefix,
+		"existing_resource_group_name": resourceGroup,
 		"sm_service_plan":              "trial",
 		"use_existing_resource_group":  false,
 		"ibmcloud_api_key":             options.RequiredEnvironmentVars["TF_VAR_ibmcloud_api_key"], // always required by the stack

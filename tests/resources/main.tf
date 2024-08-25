fc9a63fde30c806141a798222e79038bdbd34b15
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-08-25 04:03:38 +0100

    chore(deps): update terraform terraform-ibm-modules/secrets-manager/ibm to v1.17.8 (#114)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -31,7 +31,7 @@ module "event_notifications" {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "1.17.7"
+  version              = "1.17.8"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure

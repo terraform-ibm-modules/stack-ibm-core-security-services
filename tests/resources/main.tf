91d8250ab4c3cf4ef40a85f79bf248b884e9bd94
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-09-28 19:17:09 +0000

    chore(deps): update terraform terraform-ibm-modules/event-notifications/ibm to v1.10.20

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.10.19"
+  version           = "1.10.20"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags

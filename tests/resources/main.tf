2a489c712df3eac1d2f1b90a7a5c1539eb6adddf
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-02-08 04:54:24 +0000

    chore(deps): update terraform terraform-ibm-modules/event-notifications/ibm to v1.17.1

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.17.0"
+  version           = "1.17.1"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags

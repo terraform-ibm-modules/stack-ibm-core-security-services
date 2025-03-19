0b28c9320164733469d270b46974d311c26ded19
Alex Reiff <alex.reiff@ibm.com>
2025-03-19 13:27:44 -0400

    restrict tests to eu-de

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -28,7 +28,7 @@ var permanentResources map[string]interface{}
 
 // Current supported regions (NOTE: eu-es is not being used as we don't have extended trial plan quota in that region currently)
 var validRegions = []string{
-	"us-south",
+	// "us-south",
 	"eu-de",
 }
 

0d160b40b8ecb0329ea7fbc74f29ca1c9c5973a1
ocofaigh <ocofaigh@ie.ibm.com>
2025-04-01 11:48:48 +0100

    update SM VL

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.18.8"
+  version           = "1.19.8"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags

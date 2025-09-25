ab633e7db8c25824d4d059328261b0c25101c2d4
Rajat Agrawal <rajatatibm@gmail.com>
2025-09-25 15:15:31 +0100

    fully configurable

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -38,3 +38,14 @@ module "secrets_manager" {
   sm_service_plan      = "trial"
   sm_tags              = var.resource_tags
 }
+
+##############################################################################
+# Key Protect
+##############################################################################
+
+module "key_protect" {
+  source = "terraform-ibm-modules/kms-all-inclusive/ibm"
+  version= "5.2.2"
+  resource_group_id = module.resource_group.resource_group_id
+  region = var.region
+}

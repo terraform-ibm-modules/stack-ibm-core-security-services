ac48edaf74f1927299deac025df2229a6bdcdfbd
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-10-26 00:17:16 +0100

    chore(deps): update dependencies (#258)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.10.4"
+  version           = "2.10.5"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
@@ -31,7 +31,7 @@ module "event_notifications" {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "2.11.2"
+  version              = "2.11.3"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
@@ -45,7 +45,7 @@ module "secrets_manager" {
 
 module "key_protect" {
   source = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version= "5.4.3"
+  version= "5.4.5"
   resource_group_id = module.resource_group.resource_group_id
   region = var.region
 }

88b786e5333501ae86d6470f7931fb7eeb96ffd3
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-14 15:15:25 +0000

    fix(deps): update terraform terraform-ibm-modules/event-notifications/ibm to v2.10.19 (#276)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.10.18"
+  version           = "2.10.19"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags

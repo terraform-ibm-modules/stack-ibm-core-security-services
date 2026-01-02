5a20ab0badc2978504b854d908d7b830912d0778
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-02 15:05:55 +0000

    chore(deps): update dependencies

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.10.29"
+  version           = "2.10.31"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
@@ -31,7 +31,7 @@ module "event_notifications" {
 
 module "key_protect" {
   source            = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version           = "5.5.16"
+  version           = "5.5.18"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
 }

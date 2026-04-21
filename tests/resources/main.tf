81261ca7230d755f37c2c26a8d3aadd93b585378
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2026-04-22 00:12:34 +0530

    chore: update dependencies and fixed EN version in tests

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,10 +16,10 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "2.12.0"
+  version           = "2.12.6"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
-  tags              = var.resource_tags
+  resource_tags     = var.resource_tags
   plan              = "lite"
   service_endpoints = "private"
   region            = var.region
